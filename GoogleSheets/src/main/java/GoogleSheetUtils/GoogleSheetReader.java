13c50af61e0e85ca2f3ad7d2585bfd697235fa2b
unknown <vlad.raba@mail.ru>
2020-05-01 02:34:44 +0300

    refactor: google sheets service provider and reader classes

--- GoogleSheets/src/main/java/GoogleSheetUtils/GoogleSheetReader.java
+++ GoogleSheets/src/main/java/GoogleSheetUtils/GoogleSheetReader.java
@@ -1,61 +1,22 @@
 package GoogleSheetUtils;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.security.GeneralSecurityException;
 import java.util.List;
-import java.util.Arrays;
-
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.client.util.store.FileDataStoreFactory;
 
 import com.google.api.services.sheets.v4.Sheets;
-import com.google.api.services.sheets.v4.SheetsScopes;
 import com.google.api.services.sheets.v4.model.ValueRange;
 
 public class GoogleSheetReader {
-    private static Sheets sheetsService;
-    private static String APPLICATION_NAME = "Java GoogleSheets";
-    private static String SPREADSHEET_ID = "1XhBUJ2o946YUXzrPGf6kMnEjc4-RK1V7kzKYJBhmLug";
-
-    private static Credential authorize() throws IOException, GeneralSecurityException {
-        InputStream in = GoogleSheetReader.class.getResourceAsStream("/credentials.json");
-        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JacksonFactory.getDefaultInstance(), new InputStreamReader(in));
-
-        List<String> scopes = Arrays.asList(SheetsScopes.SPREADSHEETS);
-
-        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-                GoogleNetHttpTransport.newTrustedTransport(), JacksonFactory.getDefaultInstance(), clientSecrets, scopes)
-                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File("tokens")))
-                .setAccessType("offline")
-                .build();
-        Credential credential = new AuthorizationCodeInstalledApp(
-                flow, new LocalServerReceiver())
-                .authorize("user");
-
-        return credential;
-    }
-
-    public static Sheets getSheetsService() throws IOException, GeneralSecurityException {
-        Credential credential = authorize();
-        return new Sheets.Builder(GoogleNetHttpTransport.newTrustedTransport(),
-                JacksonFactory.getDefaultInstance(), credential)
-                .setApplicationName(APPLICATION_NAME)
-                .build();
-    }
+    private Sheets sheetsService;
+    private String SPREADSHEET_ID = "1XhBUJ2o946YUXzrPGf6kMnEjc4-RK1V7kzKYJBhmLug";
+    private GoogleSheetServiceProvider googleSheetServiceProvider = new GoogleSheetServiceProvider();
 
     public List<List<Object>> getSheetContent(String sheetName, String upperLeftCellName, String lowerRightCellName) throws IOException, GeneralSecurityException {
-        sheetsService = getSheetsService();
+        this.sheetsService = this.googleSheetServiceProvider.getSheetsService();
         String range = "" + sheetName + "!" + upperLeftCellName + ":" + lowerRightCellName;
 
-        ValueRange response = sheetsService.spreadsheets().values()
+        ValueRange response = this.sheetsService.spreadsheets().values()
                 .get(SPREADSHEET_ID, range)
                 .execute();
 
